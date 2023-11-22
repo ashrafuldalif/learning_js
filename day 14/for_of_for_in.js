b05a4193a09378d5a7e4fe2562eb44029e80979e
ashrafuldalif <140844323+ashrafuldalif@users.noreply.github.com>
2023-11-22 17:25:50 +0600

    Update for_of_for_in.js

--- day 14/for_of_for_in.js
+++ day 14/for_of_for_in.js
@@ -1,3 +1,26 @@
 // for of is for all arrays and 
 // for in is for object 
 
+    // FOR OF____________________________________________________________
+
+let numbers =[1,2,3,4,5,6,7,8];
+let sum=0;
+for (i of numbers){
+    i=parseInt(i);
+    sum+=i;
+    i<8?console.log(`+${i}`):console.log(`${i}`);
+}
+console.log(sum);
+
+
+let person={
+    name:`ashraful alif`,
+    age : 20,
+    "home town":"gazipur",
+    Country:"bangladesh",
+    Relegion :'islam'
+}
+
+for (x in person){
+    console.log(`${x} : ${person[x]}`);
+}
