b451a8c732725b39db7c2b419b856a1d451430b8
ashrafuldalif <ashrafulalif26@gmail.com>
2023-11-25 18:59:52 +0600

    a demo of arrow function

--- day 15/arrow_function.js
+++ day 15/arrow_function.js
@@ -19,3 +19,46 @@ console.log(sayHello());
 const add=(x,y)=>x+y;
 
 console.log(add(20,30));
+
+console.log(`\n a demo of usefulness of arrow function \n\n\n`);
+
+let students=[      // an array of objects by using {2nd brackets} inside of  [3rd bracket] 
+{
+    name:"ashraful",
+    roll:3009,
+    gpa:3.69
+},
+{
+    name:"hoob",
+    roll:3069,
+    gpa:3.50
+},
+{
+    name:"luffy",
+    roll:3010,
+    gpa:2.69
+},
+{
+    name:"sabo",
+    roll:3019,
+    gpa:3.99
+},
+{
+    name:"ace",
+    roll:3001,
+    gpa:2.85
+}
+]
+function nameOfToppers1(){
+return students.filter(function(x){
+    return x.gpa>3;
+    }).map(function (y){
+        return y.name;
+    })
+}
+
+console.log(`With normal function ::: ${nameOfToppers1()}`);
+
+const nameOfToppers2 = () =>students.filter(a=> a.gpa>3).map(b => b.name);
+
+console.log(`with arrow function ::: ${nameOfToppers2()}`);
