7f3e6265c0cd9bdde7a04744e8a88b8fe925df86
ashrafuldalif <ashrafulalif26@gmail.com>
2023-11-25 18:08:51 +0600

    arrow function

---
+++ day 15/arrow_function.js
@@ -0,0 +1,21 @@
+// function sayHello() {
+//     console.log("hello, i am ashraful d alif");
+// }
+const example=(x,y)=>{
+    console.log("this is an example how to create an arrow function with so many statments ")
+    console.log(`${x}+${y}=${x+y}`);
+}
+example(12,35);
+
+const hello=()=>console.log("hello , i ma ashraful d alif without with console log" )
+hello()
+const sayHello=()=>"hello , i ma ashraful d alif with out console log"
+console.log(sayHello());
+
+// function add (x,y){
+//     return x+y;
+// }
+
+const add=(x,y)=>x+y;
+
+console.log(add(20,30));
